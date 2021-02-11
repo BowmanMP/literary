428f0e43ca0866684ff103318d86db0b8a7ad0c0
Matt Bowman <31557759+BowmanMP@users.noreply.github.com>
2021-02-11 15:45:36 -0500

    Update SearchString.js

--- SearchString.js
+++ SearchString.js
@@ -1,7 +1,7 @@
 //Load a book from disk
 function loadBook(filename, displayName) {
     let currentBook = "";
-    let url = "blob/master/" + filename;
+    let url = "master/" + filename;
 
     //reset our UI
     document.getElementById("fileName").innerHTML = displayName;
